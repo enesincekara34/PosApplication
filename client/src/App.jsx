ce882bd0ed4c9a8ac86776d955b7631fa4b52da8
enesincekara <enes.incekara@pixasoftware.com>
2024-03-11 13:01:38 +0300

    Header Kurulumu Yapıldı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,14 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Header from './components/Header/Header'
 
 function App() {
 
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <div>
+      <Header/>
+      home
+    </div>
+    
   )
 }
 
