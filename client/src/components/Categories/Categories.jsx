9a8f17da8a56a0a3ee6e9f57f9c3606ad5f0846e
enesincekara <enesincekara3457@gmail.com>
2024-03-12 03:51:53 +0300

    Update Categories.jsx

--- client/src/components/Categories/Categories.jsx
+++ client/src/components/Categories/Categories.jsx
@@ -39,9 +39,7 @@ const Categories = () => {
             <li className='bg-green-700 px-10 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
                 <span>İçecek</span>
             </li>
-            <li className='bg-green-700 px-10 py-10 text-white cursor-pointer hover:bg-pink-700 transition-all text-center'>
-                <span>İçecek</span>
-            </li>
+            
         </ul>
     )
 }
