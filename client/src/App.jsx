2f3a76712bd4b7afcf514bfb716ae34efb2a3001
enesincekara <enes.incekara@pixasoftware.com>
2024-03-12 03:33:02 +0300

    Categories componenti yapıldı

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Header from './components/Header/Header'
+import Categories from './components/Categories/Categories'
 
 function App() {
 
@@ -10,7 +11,17 @@ function App() {
   return (
     <div>
       <Header/>
-      home
+      <div className="home px-6 flex justify-between gap-10">
+        <div className="categories flex-1 h-screen overflow-y-scroll pb-64">
+          <Categories/>
+        </div>
+        <div className="products flex-[8]">
+          <div>products</div>
+        </div>
+        <div className="cartTotals">
+          <div>cart totals</div>
+        </div>
+      </div>
     </div>
     
   )
