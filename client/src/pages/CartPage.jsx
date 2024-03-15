1100b724ad062bc188c47dec2cc5b8c6d9c2991f
enesincekara <enesincekara3457@gmail.com>
2024-03-15 19:52:59 +0300

    Cart Sayfası

--- client/src/pages/CartPage.jsx
+++ client/src/pages/CartPage.jsx
@@ -1,9 +1,76 @@
 import React from "react";
+import Header from "../components/Header/Header";
+import { Table, Card, Button} from "antd";
+import { useState } from "react";
+import CreateBill from "../components/Cart/CreateBill";
 
 const CartPage = () => {
-    return(
-        <div>Cart Page</div>
-    )
-}
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const dataSource = [
+    {
+      key: "1",
+      name: "Mike",
+      age: 32,
+      address: "10 Downing Street",
+    },
+    {
+      key: "2",
+      name: "John",
+      age: 42,
+      address: "10 Downing Street",
+    },
+  ];
 
-export default CartPage
+  const columns = [
+    {
+      title: "Name",
+      dataIndex: "name",
+      key: "name",
+    },
+    {
+      title: "Age",
+      dataIndex: "age",
+      key: "age",
+    },
+    {
+      title: "Address",
+      dataIndex: "address",
+      key: "address",
+    },
+  ];
+
+  return (
+    <div>
+      <Header />
+      <div className="px-6">
+        <Table dataSource={dataSource} columns={columns} bordered />
+        <div className="cart-total flex justify-end">
+          <Card className="w-72 mt-4">
+            <div className="flex justify-between">
+              <span>Ara Toplam</span>
+              <span>547.98₺</span>
+            </div>
+            <div className="flex justify-between my-2">
+              <span>KDV Toplam %8</span>
+              <span className="text-red-600">+43.92₺</span>
+            </div>
+            <div className="flex justify-between">
+              <b>Toplam</b>
+              <b>592.92₺</b>
+            </div>
+            <Button
+              className="mt-4 w-full text-center bg-blue-600 hover:bg-white"
+              size="large"
+              onClick={() => setIsModalOpen(true)}
+            >
+              Sipariş Oluştur
+            </Button>
+          </Card>
+        </div>
+      </div>
+      <CreateBill isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen}/>
+    </div>
+  );
+};
+
+export default CartPage;
